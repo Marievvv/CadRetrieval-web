2a5701d447d748c6a28e8182ce6a35286c7ed28f
Marievvv <mari.volkova.2005@list.ru>
2025-07-07 11:58:38 +0300

    Set structure

--- example_retrieval.py
+++ example_retrieval.py
@@ -1,18 +1,16 @@
-from uvnet.models import Contrast
-from retrieval.vector_db import VectorDatabase
+from model.uvnet.models import Contrast
+from model.retrieval.vector_db import VectorDatabase
 from dgl.data.utils import load_graphs
 from torch import FloatTensor
-from datasets import util
+from model.datasets import util
 
 model_weights = "./results/0701/095827/best.ckpt"
 vector_db_folder = "./vector_db/1"
 vector_db_name = "FaBWave"
 cad_file = "./data/FABWave/Holebolts_With_Shoulders/bin/e075dc78-4a73-4e53-b6a6-b25f48e6830d.bin" # bin file
 
-
 db = VectorDatabase(vector_db_folder, vector_db_name)
 model = Contrast.load_from_checkpoint(model_weights)
-
 
 graph = load_graphs(cad_file)[0][0]
 # center_and_scale
